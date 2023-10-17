161ce1a9205bacc8d2887ad28b9c718125c1ed29
daigoro <daigoropersson@gmail.com>
2023-10-17 13:49:39 +0200

    login/signup/forgot password

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
+import ForgotPassScreen from './screens/ForgotPassScreen';
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+      >        
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Forgot" component={ForgotPassScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
---
+++ frontend/screens/ForgotPassScreen.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
+
+export default function ForgotPassScreen({ navigation }) {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.header}>Hitch Hike</Text>
+        <View style={styles.inputContent}>
+          <TextInput
+            style={styles.inputs}
+            onChangeText={(text) => setEmail(text)}
+            value={email}
+            placeholder="Enter Email"
+          />
+          <TouchableOpacity 
+            style={styles.loginOption}
+            onPress={() => (navigation.navigate("Login"))}
+          >
+            <Text>Login</Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity
+          style={styles.loginButton}
+          // onPress={() => }
+        >
+          <Text style={styles.loginText}>Get Password </Text>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  content: {
+    flex: 0.7,
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  header: {
+    fontSize: 60,
+    fontWeight: "700",
+    textAlign: "center",
+    color: 'black',
+  },
+  inputs: {
+    width: 230,
+    borderColor: '#D7D7D7',
+    borderWidth: 2,
+    padding: 12,
+    borderRadius: 40,
+  },
+  inputContent: {
+    flex: 0.5,
+    alignItems: 'center',
+    justifyContent: 'space-evenly'
+  },
+  loginOption: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#E9A738',
+  },
+  loginButton: {
+    padding: 18,
+    borderRadius: 15,
+    width: 200,
+    backgroundColor: "#E9A738",
+  },
+  loginText: {
+    fontSize: 20,
+    fontWeight: "600",
+    textAlign: "center",
+    color: 'white',
+  }
+});
